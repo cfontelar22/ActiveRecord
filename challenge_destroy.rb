571c035f141033455d12ae145dcffb9334f7bb35
cfontelar <fontelarcj@gmail.com>
2024-01-29 14:39:34 -0600

    Initial commit

---
+++ challenge_destroy.rb
@@ -0,0 +1,17 @@
+
+
+require_relative 'ar'
+require_relative 'models/product'
+
+# Find a product by its ID and delete it from the database
+product_id_to_delete = 11 
+
+product_to_delete = Product.find_by(id: product_id_to_delete)
+
+if product_to_delete
+  puts "Product found. Deleting #{product_to_delete.name}..."
+  product_to_delete.destroy
+  puts "Product deleted successfully."
+else
+  puts "Product not found."
+end
---
+++ challenge_update.rb
@@ -0,0 +1,26 @@
+# challenge_update.rb
+
+require_relative 'ar'
+require_relative 'models/product'
+
+# Find all products with a stock quantity greater than 40
+products_greater_than_40 = Product.where("stock_quantity > 40")
+
+if products_greater_than_40.any?
+  puts "Products with stock quantity greater than 40:"
+  products_greater_than_40.each do |product|
+    puts "- #{product.name}: #{product.stock_quantity}"
+  end
+  
+  puts "\nUpdating stock quantity..."
+  
+  # Iterate over each product and increment the stock quantity by 1
+  products_greater_than_40.each do |product|
+    product.stock_quantity += 1
+    product.save
+  end
+  
+  puts "Stock quantities updated successfully."
+else
+  puts "No products found with stock quantity greater than 40."
+end
